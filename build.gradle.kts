3f1cc2961322788dcda62b56c70dc6039c12ddfd
ts0687501-warner <ts0687501@gmail.com>
2026-08-02 13:17:08 +0000

    Fix Compose plugin compatibility with Kotlin 1.9.24

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("com.android.library") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
 }
 
 tasks.register("clean", Delete::class) {
